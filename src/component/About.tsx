5fadd5937d8dc9323c0bedc5a3f5557530bac094
blurymind@gmail.com <blurymind@gmail.com>
2023-07-09 20:57:08 +0100

    add a A2HS button

--- src/component/About.tsx
+++ src/component/About.tsx
@@ -1,8 +1,12 @@
+import React from "react";
 
 import "../App.css";
+import A2HSButton from "../A2HSButton";
+
 const About = ({aboutText, appName, children}: any) => {
     return (
         <div style={{display: "flex", justifyContent: "center", width: "100%"}}>
+            <A2HSButton/>
             <div className="about-message">
                 <div className="app-logo" title={aboutText}>{appName}</div>
                 <p>{aboutText}</p>
